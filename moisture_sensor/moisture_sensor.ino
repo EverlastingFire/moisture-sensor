48bc74e5862c3653f4b824291a3296f3295b49ce
marconapalm <marconapalm@gmail.com>
2013-11-29 00:09:29 +0100

    New branch and code updated with SOS

--- moisture_sensor/moisture_sensor.ino
+++ moisture_sensor/moisture_sensor.ino
@@ -12,7 +12,8 @@ static int LED_PIN = 13;              // LED pin number
 static int threshold = 800;           // threshold for activating the LED
 
 static long SPEED=9600;               // speed of serial data's transmission
-static long interval=300000;          // interval of 5 minutes in milliseconds
+static long interval_1m=60000;        // interval of 1 minute in milliseconds
+static long interval_30m=1800000;     // interval of 30 minutes in milliseconds
 
 static char message[]="Feed me!";     // message to print on the monitor
 /*static char message2[]="You can change the threshold. The current value is: ";*/
@@ -26,18 +27,46 @@ void setup()
     Serial.begin(SPEED);              // start sending and receiving serial data
 }
 
+void ledSOS(){
+  int i = 0;
+  for (i=0; i<3; i++){            // letter S, made by 3 dots
+    digitalWrite(LED_PIN, HIGH);   // turns on the LED
+    delay(250);                   // wait
+    digitalWrite(LED_PIN, LOW);    // turns off the LED
+    delay(250);
+  }
+  delay(1500);
+  i = 0;
+  for (i=0; i<3; i++){            // letter O, made by 3 dashes
+    digitalWrite(LED_PIN, HIGH); 
+    delay(900); 
+    digitalWrite(LED_PIN, LOW); 
+    delay(250);
+  }
+  delay(1500);
+  i = 0;
+  for (i=0; i<3; i++){            // letter S, made by 3 dots
+    digitalWrite(LED_PIN, HIGH); 
+    delay(250); 
+    digitalWrite(LED_PIN, LOW); 
+    delay(250);
+  }
+}
+
 void loop()
 {
-  
   reading = analogRead(SENSOR_PIN);   // get the sensor reading 
   led_state = digitalRead(LED_PIN);   // get the current LED state
   
-  while (reading<=threshold){         // until the recorded value is below 800
-    digitalWrite(LED_PIN, HIGH);      // turn on the LED
+  if (reading<=threshold){            // if the recorded value is below 800
+    void ledSos();                    // launch the SOS light message
     Serial.println(message);          // print the message to the monitor
-    delay(interval);                  // wait for 5 minutes
+    void ledSos();                    // re-launch the SOS light message
+    delay(interval_1m);               // wait 1 minute
+  }
+  else{                               // if the plant soil is moist wait a bit
     digitalWrite(LED_PIN, LOW);       // turn off the LED
-    reading = analogRead(SENSOR_PIN); // get the sensor reading another time
+    delay(interval_30m);              // wait 30 minutes
   }
   
   /* TODO: allow the user changing the threshold value*/
